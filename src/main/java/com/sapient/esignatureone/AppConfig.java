57593a7f1a7b7869dd0c5b3764b5cf9e7bd6d007
David Wolfand <dwolfand@sapient.com>
2014-04-22 17:53:15 -0400

    initial commit

---
+++ src/main/java/com/sapient/esignatureone/AppConfig.java
@@ -0,0 +1,14 @@
+package com.sapient.esignatureone;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class AppConfig {
+
+}
---
+++ src/main/java/com/sapient/esignatureone/WebAppInitializer.java
@@ -0,0 +1,24 @@
+package com.sapient.esignatureone;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { RepositoryRestMvcConfiguration.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
